c526b8227701309e86d330da00080878d4fbaff9
Mari Guerrieri <mario@guerrieri.codes>
2026-09-16 13:04:57 -0700

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        partials_forward = [1] * len(nums)
+        for i, n in islice(enumerate(nums), 0, len(nums) - 1):
+            partials_forward[i + 1] = partials_forward[i] * n
+
+        partials_reverse = [1] * len(nums)
+        for i, n in islice(reversed(list(enumerate(nums))), 0, len(nums) - 1):
+            partials_reverse[i - 1] = partials_reverse[i] * n
+
+        return [before * after
+                for before, after
+                in zip(partials_forward, partials_reverse)]
